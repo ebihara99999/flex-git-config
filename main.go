b7837fbf5e11831dcb591f14c999df8a7c07b86d
Yusuke Ebihara <dev.biibiebi@gmail.com>
2018-10-13 12:45:11 +0900

    Set email and username in the specified domain's directories

--- main.go
+++ main.go
@@ -2,42 +2,96 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 var name = flag.String("name", "World", "A name to say hello to")
 
 var username string
 var email string
+var domain string
+var noop bool
 
 func init() {
 	flag.StringVar(&username, "username", "", "Set Github/GHE username")
 	flag.StringVar(&username, "u", "", "Set Github/GHE username")
 	flag.StringVar(&email, "email", "", "Set Github/GHE email")
 	flag.StringVar(&email, "e", "", "Set Github/GHE email")
+	flag.StringVar(&domain, "domain", "", "Set Github/GHE domain")
+	flag.StringVar(&domain, "d", "", "Set Github/GHE domain")
+	flag.BoolVar(&noop, "n", false, "noop")
+
 }
 
 func main() {
 	flag.Parse()
 
-	usage := "Usage: hoge -u username -e email -"
+	// Check the args
+	usage := "Usage: hoge -u username -e email -d domain"
 	if username == "" {
 		panic(usage)
-	}
-
-	errUsername := exec.Command("git", "config", "--local", "user.name", username).Run()
-
-	if errUsername != nil {
-		panic(errUsername)
 	}
 
 	if email == "" {
 		panic(usage)
 	}
 
-	errEmail := exec.Command("git", "config", "--local", "user.email", email).Run()
+	if domain == "" {
+		panic(usage)
+	}
 
-	if errEmail != nil {
-		panic(errEmail)
+	// ghq listして d/user/package のドメインが一致するものを持ってきて、cd して git configする
+	repositories := []string{}
+	tmpRepositories, _ := exec.Command("ghq", "list").Output()
+	repositories = strings.Split(string(tmpRepositories), "\n")
+	matchedRepositories := []string{}
+	_ghqRoot, _ := exec.Command("ghq", "root").Output()
+	ghqRoot := string(_ghqRoot)
+	ghqRoot = strings.TrimRight(ghqRoot, "\n")
+	r := regexp.MustCompile(domain)
+	for _, repo := range repositories {
+		if r.MatchString(repo) {
+			_absPath := path.Join(ghqRoot, repo)
+			matchedRepositories = append(matchedRepositories, _absPath)
+		}
+	}
+
+	// For debug
+	if noop {
+		fmt.Println("Noop operation.")
+		fmt.Println("The taget repositories are:")
+		for _, targetDir := range matchedRepositories {
+			fmt.Println(targetDir)
+		}
+		os.Exit(0)
+	}
+
+	// ディレクトリの移動
+	prevDir, _ := filepath.Abs(".")
+	defer os.Chdir(prevDir)
+
+	for _, targetDir := range matchedRepositories {
+		err := os.Chdir(targetDir)
+		if err != nil {
+			panic(err)
+		}
+
+		errUsername := exec.Command("git", "config", "--local", "user.name", username).Run()
+
+		if errUsername != nil {
+			panic(errUsername)
+		}
+
+		errEmail := exec.Command("git", "config", "--local", "user.email", email).Run()
+
+		if errEmail != nil {
+			panic(errEmail)
+		}
 	}
 }
