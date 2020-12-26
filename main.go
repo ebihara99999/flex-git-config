0c9f25602eef360c5ea87459170c6c53640c774d
ebihara99999 <dev.biibiebi@gmail.com>
2020-12-27 00:27:46 +0900

    Temporarily merge commands.go into main.go to pass the tests

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"os/exec"
 )
 
 var username string
@@ -113,4 +114,44 @@ func checkArgs() (result bool, err error) {
 	return true, nil
 }
 
+// Commands interface is
+type Commands interface {
+	GetRepositoriesByGhqList() ([]byte, error)
+	GetGhqRoot() ([]byte, error)
+	ChangeGitUsernameLocally(username string) ([]byte, error)
+	ChangeGitEmailLocally(email string) ([]byte, error)
+}
 
+// Commander is 
+type Commander struct {
+	c Commands
+}
+
+// GetRepositoriesByGhqList is
+func (c *Commander) GetRepositoriesByGhqList() ([]byte, error) {
+	fmt.Println("hogehogehogehogehogehogeho")
+	cmd := exec.Command("ghq", "list")
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
+
+// GetGhqRoot is
+func (c *Commander) GetGhqRoot() ([]byte, error) {
+	cmd := exec.Command("ghq", "root")
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
+
+// ChangeGitUsernameLocally is
+func (c *Commander) ChangeGitUsernameLocally(username string) ([]byte, error) {
+	cmd := exec.Command("git", "config", "--local", "user.name", username)
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
+
+// ChangeGitEmailLocally is
+func (c *Commander) ChangeGitEmailLocally(email string) ([]byte, error) {
+	cmd := exec.Command("git", "config", "--local", "user.email", email)
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
